48d7b53b949defbf243447f370a53532cfe04c39
deniexx <deniexxx17@gmail.com>
2022-03-01 22:54:53 +0000

    Improved PlayerMovement and Added sprites!

--- RahmivasionUnity/Assets/Rahmivasion/Scripts/PlayerMovement.cs
+++ RahmivasionUnity/Assets/Rahmivasion/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,52 +6,112 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private Rigidbody2D _rb;
-
-    [SerializeField] private float maxSpeed = 4.0f;
-    [SerializeField] private float speed = 2.0f;
-    [SerializeField] private float jumpVelocity = -1.0f;
-    [SerializeField] private float canJumpTime = 1.25f;
-
-    private float jumpTime = 0.0f;
-
-    private Vector2 newVelocity;
-
-    void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        ProcessInput();
+    [SerializeField] private LayerMask lmWalls;
         
-    }
+        [SerializeField] private float fJumpVelocity = 5f;
+    
+        private Rigidbody2D _rb;
 
-    void ProcessInput()
-    {
-        float horizontalAxis = Input.GetAxisRaw("Horizontal");
-        bool wantsToJump = Input.GetButtonDown("Jump");
-
-        float delta = Time.deltaTime;
-
-        float velocityX = Mathf.Clamp(horizontalAxis * speed, -maxSpeed, maxSpeed);
-        float velocityY = _rb.velocity.y;
-
-
-        if (wantsToJump && jumpTime <= 0.0f)
+        private bool flipped;
+        
+        private float fJumpPressedRemember = 0;
+        [SerializeField] private float fJumpPressedRememberTime = 0.1f;
+    
+        private float fGroundedRemember = 0f;
+        [SerializeField] private float fGroundedRememberTime = 0.15f;
+    
+        [SerializeField] private float fHorizontalAcceleration = 0.25f;
+    
+        [SerializeField] [Range(0, 1)] private float fHorizontalDampingBasic = 0.5f;
+        [SerializeField] [Range(0, 1)] private float fHorizontalDampingWhenStopping = 0.7f;
+        [SerializeField] [Range(0, 1)] private float fHorizontalDampingWhenTurning = 0.5f;
+        
+        [SerializeField] [Range(0, 1)] private float fJumpCutHeight = 0.5f;
+    
+        [SerializeField] private float fMaxSpeed = 4f;
+        
+        void Start()
         {
-            velocityY = jumpVelocity;
-            jumpTime = canJumpTime;
+            _rb = GetComponent<Rigidbody2D>();
+        }
+        
+        void Update()
+        {
+            Vector2 v2GroundedBoxCheckPosition = (Vector2) transform.position + new Vector2(0, -0.5f);
+            Vector2 v2GroundedBoxCheckScale = (Vector2) transform.localScale + new Vector2(-0.02f, 0);
+            bool bGrounded = Physics2D.OverlapBox(v2GroundedBoxCheckPosition, v2GroundedBoxCheckScale, 0, lmWalls);
+
+            fGroundedRemember -= Time.deltaTime;
+            if (bGrounded) fGroundedRemember = fGroundedRememberTime;
+    
+            fJumpPressedRemember -= Time.deltaTime;
+            if (Input.GetButtonDown("Jump")) fJumpPressedRemember = fJumpPressedRememberTime;
+
+            if (Input.GetButtonUp("Jump"))
+            {
+                if (_rb.velocity.y > 0)
+                {
+                    _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * fJumpCutHeight);
+                }
+            }
+               
+    
+            if ((fJumpPressedRemember > 0) && (fGroundedRemember > 0))
+            {
+                fJumpPressedRemember = 0;
+                fGroundedRemember = 0;
+                _rb.velocity = new Vector2(_rb.velocity.x, fJumpVelocity);
+            }
+            
+            float fHorizontalVelocity = _rb.velocity.x;
+            float inputStrength = Input.GetAxisRaw("Horizontal");
+            fHorizontalVelocity += inputStrength * fHorizontalAcceleration;
+    
+            if (Math.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f)
+                fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenStopping, Time.deltaTime * 10f);
+            else if ((Mathf.Sign(Input.GetAxisRaw("Horizontal")) != Mathf.Sign(fHorizontalVelocity)))
+                fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenTurning, Time.deltaTime * 10f);
+            else
+                fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingBasic, Time.deltaTime * 10f);
+    
+            fHorizontalVelocity = Mathf.Clamp(fHorizontalVelocity, -fMaxSpeed, fMaxSpeed);
+    
+            AnimateCharacter(inputStrength);
+            
+            _rb.velocity = new Vector2(fHorizontalVelocity, _rb.velocity.y);
+    
+            if (Mathf.Abs(_rb.velocity.x) < 0.05)
+            {
+                _rb.velocity = new Vector2(0.0f, _rb.velocity.y);
+            }
+        }
+    
+        private void AnimateCharacter(float inputStrength)
+        {
+            if (inputStrength < -0.01f)
+            {
+                Flip(true);
+            }
+            else if (inputStrength > 0.01f)
+            {
+                Flip(false);
+            }
+        }
+    
+        private void Flip(bool flip)
+        {
+            transform.localScale = flip ? new Vector3(-1.0f, 1.0f, 1.0f) : new Vector3(1.0f, 1.0f, 1.0f);
+
+            flipped = flip;
         }
 
-        jumpTime = Mathf.Clamp(jumpTime - delta, 0.0f, canJumpTime);
-
-        _rb.velocity = new Vector2(velocityX, velocityY);
-
-        if (horizontalAxis == 0)
+        public bool GetIsPlayerFlipped()
         {
-            _rb.velocity = Vector2.zero;
+            return flipped;
         }
-    }
+    
+        public void StopPlayerInPlace()
+        {
+            _rb.velocity = new Vector2(0.0f, 0.0f);
+        }
 }
