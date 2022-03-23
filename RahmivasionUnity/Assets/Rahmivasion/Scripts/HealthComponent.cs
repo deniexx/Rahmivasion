8d4744d68c55b7fffc3d91276dc90b1c7dd81881
deniexx <deniexxx17@gmail.com>
2022-03-23 10:51:11 +0000

    Added summaries to functions in static library and health component

--- RahmivasionUnity/Assets/Rahmivasion/Scripts/HealthComponent.cs
+++ RahmivasionUnity/Assets/Rahmivasion/Scripts/HealthComponent.cs
@@ -17,7 +17,13 @@ public class HealthComponent : MonoBehaviour
     {
         currentHealth = baseHealth;
     }
-
+    
+    /// <summary>
+    /// Applies change to health based on delta
+    /// </summary>
+    /// <param name="instigator">This is the gameObject that has called this event</param>
+    /// <param name="delta">The difference in health to be applied, should be negative number if you want to apply damage</param>
+    /// <returns></returns>
     public bool ApplyHealthChange(GameObject instigator, float delta)
     {
         float oldHealth = currentHealth;
@@ -30,26 +36,46 @@ public class HealthComponent : MonoBehaviour
         return actualDelta != 0;
     }
 
+    /// <summary>
+    /// Kills the gameObject, sets it's health to 0
+    /// </summary>
+    /// <returns></returns>
     public void Kill()
     {
         ApplyHealthChange(this.gameObject, -maxHealth);
     }
-
+    
+    /// <summary>
+    /// Checks if the gameObjects Health is max
+    /// </summary>
+    /// <returns>True if player is at max health and false if he's not</returns>
     public bool IsAtMaxHealth()
     {
         return currentHealth == maxHealth;
     }
 
+    /// <summary>
+    /// Returns the max health of the player
+    /// </summary>
+    /// <returns>Float value of the gameObject's MaxHealth</returns>
     public float GetMaxHealth()
     {
         return maxHealth;
     }
-
+    
+    /// <summary>
+    /// Gets the player of the health
+    /// </summary>
+    /// <returns>Flaot value of the gameObject's health</returns>
     public float GetHealth()
     {
         return currentHealth;
     }
 
+    /// <summary>
+    /// Checks if gameObject's health is more than 0
+    /// </summary>
+    /// <returns>True if gameObject's health is more than 0, else false</returns>
     public bool IsAlive()
     {
         return currentHealth > 0.0f;
--- RahmivasionUnity/Assets/Rahmivasion/Scripts/RahmivasionStaticLibrary.cs
+++ RahmivasionUnity/Assets/Rahmivasion/Scripts/RahmivasionStaticLibrary.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class RahmivasionStaticLibrary : MonoBehaviour
 {
+    /// <summary>
+    /// Returns the health component of a selected game object
+    /// </summary>
+    /// <param name="gameObject">The gameObject to take attemp to get the health component from</param>
+    /// <returns>The gameObject's healthComponent</returns>
     public static HealthComponent GetHealthComponent(GameObject gameObject)
     {
         return gameObject.GetComponent<HealthComponent>();
     }
-
+    
+    /// <summary>
+    /// Checks if the gameObject is alive
+    /// </summary>
+    /// <param name="gameObject">The gameObject that should be checked if it is alive</param>
+    /// <returns>Returns wherether the game object is alive, if it does not have HealthComponent, return value will be false</returns>
     public static bool IsGameObjectAlive(GameObject gameObject)
     {
         HealthComponent healthComp = gameObject.GetComponent<HealthComponent>();
@@ -21,6 +31,10 @@ public class RahmivasionStaticLibrary : MonoBehaviour
         return false;
     }
 
+    /// <summary>
+    /// Kills the selected GameObject
+    /// </summary>
+    /// <param name="gameObject">GameObject to kill</param>
     public static void KillGameObject(GameObject gameObject)
     {
         HealthComponent healthComp = gameObject.GetComponent<HealthComponent>();
@@ -31,6 +45,13 @@ public class RahmivasionStaticLibrary : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Applies health change to a game object
+    /// </summary>
+    /// <param name="instigator">The gameObject that is calling this event</param>
+    /// <param name="gameObjectToDamge">The gameObject that should be damaged</param>
+    /// <param name="delta">The health delta to be applied, must be negative to apply damage</param>
+    /// <returns>Return true if health change has been sucessfully applied, ie health has been changed</returns>
     public static bool ApplyGameObjectHealthChange(GameObject instigator, GameObject gameObjectToDamge, float delta)
     {
         HealthComponent healthComp = gameObjectToDamge.GetComponent<HealthComponent>();
